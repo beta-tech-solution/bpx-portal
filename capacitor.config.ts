2a14e3aeb33b5c2ce239bf506e404b78b7c1da1e
Abdul Rehman <t4tech2011@gmail.com>
2025-08-05 13:59:04 +0000

    what you will do to make APK and IOS and what i will have to do?? i don

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bpxmaster.app',
+  appName: 'BPX Master',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
