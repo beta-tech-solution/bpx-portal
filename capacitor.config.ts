d754718251af2e15a4c0bbc2751fe22b0c13b202
Abdul Rehman <t4tech2011@gmail.com>
2025-08-06 23:17:44 +0000

    ├ ○ /about-us                            2.48 kB         296 kB ├ ○ /adm

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bpxmaster.app',
   appName: 'BPX Master',
-  webDir: '.next',
+  webDir: 'out',
   bundledWebRuntime: false,
 };
 
